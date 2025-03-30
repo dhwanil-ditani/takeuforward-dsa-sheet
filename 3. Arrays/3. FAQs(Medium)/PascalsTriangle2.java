664b74c554cdcb617a28d88612d0830df9bc87d8
Dhwanil Ditani <ditanidhwanil@gmail.com>
2025-03-30 19:08:03 +0530

    update

--- 3. Arrays/3. FAQs(Medium)/PascalsTriangle2.java
+++ 3. Arrays/3. FAQs(Medium)/PascalsTriangle2.java
@@ -1,3 +1,19 @@
+class Solution {
+    public int[] pascalTriangleII(int r) {
+        int[] result = new int[r];
+        for (int i=0; i<r; i++) {
+            if (i == 0 || i == r-1) {
+                result[i] = 1;
+            } else if (i < r/2 || (r % 2 == 1 && i == r/2)) {
+                result[i] = result[i-1] * (r - i) / i;
+            } else {
+                result[i] = result[r-i-1];
+            }
+        }
+        return result;
+    }
+}
+
 public class PascalsTriangle2 {
     
 }
