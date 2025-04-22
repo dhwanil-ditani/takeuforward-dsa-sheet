607aafd3ba7806c5b25b07227db411d0a6c91ab6
Dhwanil Ditani <ditanidhwanil@gmail.com>
2025-04-22 04:02:12 +0000

    update

--- 3. Arrays/4. FAQs(Hard)/MajorityElement1.java
+++ 3. Arrays/4. FAQs(Hard)/MajorityElement1.java
@@ -1,3 +1,35 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        int count = 0;
+        int element = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (count == 0) {
+                count = 1;
+                element = nums[i];
+            } else if (element == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        count = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == element) {
+                count++;
+            }
+        }
+
+        if (count > (n / 2)) {
+            return element;
+        }
+
+        return -1;
+    }
+}
+
 public class MajorityElement1 {
-    
+
 }
