b6d401f8f852699043ed066848fcf3ebc742144b
Dhwanil Ditani <ditanidhwanil@gmail.com>
2025-06-30 01:47:38 +0530

    update

--- Sorting/Algorithms/Insertion Sort/InsertionSort.java
+++ Sorting/Algorithms/Insertion Sort/InsertionSort.java
@@ -1,15 +1,3 @@
-/**
- * <h3>Intuition</h3>
- * <p>
- * Insertion sort builds a sorted array one element at a time by repeatedly
- * picking the next element and inserting it into its correct position within
- * the already sorted part of the array.
- * </p>
- * <h3>Approach</h3>
- * <p>
- * 
- * </p>
- */
 class Solution {
     public int[] insertionSort(int[] nums) {
         int n = nums.length;
--- Sorting/Algorithms/Insertion Sort/README.md
+++ Sorting/Algorithms/Insertion Sort/README.md
@@ -1,4 +1,4 @@
 # Intuition
+Insertion sort builds a sorted array one element at a time by repeatedly picking the next element and inserting it into its correct position within the already sorted part of the array.
 
 # Approach
-
