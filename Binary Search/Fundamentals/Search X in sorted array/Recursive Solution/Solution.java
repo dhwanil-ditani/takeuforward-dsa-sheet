890964ac9c1b19da4f04f59ed7bd9155aba722fd
Dhwanil Ditani <ditanidhwanil@gmail.com>
2025-09-03 21:27:58 +0530

    update

---
+++ Binary Search/Fundamentals/Search X in sorted array/Recursive Solution/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int binarySearch(int[] nums, int low, int high, int target) {
+        if (low > high) return -1;
+
+        int mid = low + ((high - low) / 2);
+
+        if (nums[mid] == target) return mid;
+
+        if (nums[mid] > target) {
+            return binarySearch(nums, low, mid - 1, target);
+        }
+
+        return binarySearch(nums, mid + 1, high, target);
+    }
+
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+
+        return binarySearch(nums, 0, n-1, target);
+    }
+}
