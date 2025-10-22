73634fe6c29b48145087b3e6a7f58d173c9bfea4
Dhwanil Ditani <ditanidhwanil@gmail.com>
2025-10-22 06:10:59 +0530

    update

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tracking
+
+- [ ] Beginner Problems
+  - [ ] Language Basics
+  - [ ] Logic Building
+  - [ ] Patterns
+  - [ ] Time Complexity
+  - [ ] STL and Collections
+  - [ ] Concept Basics
+- [ ] Sorting
+  - [ ] Algorithms
+    - [ ] Selection Sort
+    - [ ] Bubble Sort
+    - [ ] Insertion Sorting
+    - [ ] Merge Sorting
+    - [ ] Quick Sorting
+- [ ] Arrays
+- [ ] Hashing
+- [ ] Binary Search
--- Sorting/Algorithms/Selection Sort/Solution.java
+++ Sorting/Algorithms/Selection Sort/Solution.java
@@ -1,5 +1,47 @@
-class Solution {
-    public int[] selectionSort(int[] nums) {
+import java.util.Scanner;
 
+class Solution {
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public int[] selectionSort(int[] nums) {
+        int n = nums.length;
+
+        for (int i=0; i<n; i++) {
+            int min = i;
+            for (int j=i; j<n; j++) {
+                if (nums[j] < nums[min]) {
+                    min = j;
+                }
+            }
+            if (min != i) {
+                swap(nums, i, min);
+            }
+        }
+
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String[] tokens = in.nextLine().split(" ");
+        int[] nums = new int[tokens.length];
+
+        for (int i=0; i<nums.length; i++) {
+            nums[i] = Integer.parseInt(tokens[i]);
+        }
+
+        int[] output = new Solution().selectionSort(nums);
+
+        for (int i=0; i<output.length; i++) {
+            System.out.print(output[i] + " ");
+        }
+        System.out.println();
+
+        in.close();
     }
 }
