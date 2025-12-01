2dd829da9f2400b15587a0e6d06fe70797088b81
Dhwanil Ditani <44235738+dhwanil-ditani@users.noreply.github.com>
2025-12-01 11:55:43 +0000

    update

---
+++ Arrays/Fundamentals/Left Rotate Array by One/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void rotateArrayByOne(int[] nums) {
+        int n = nums.length;
+        int start = nums[0];
+        for (int i = 1; i < n; i++) {
+            nums[i - 1] = nums[i];
+        }
+        nums[n - 1] = start;
+    }
+}
