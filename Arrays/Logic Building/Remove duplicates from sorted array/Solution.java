8804839ae8e6139b0145115a355d438717ebaaff
Dhwanil Ditani <44235738+dhwanil-ditani@users.noreply.github.com>
2025-12-03 10:26:51 +0000

    update

---
+++ Arrays/Logic Building/Remove duplicates from sorted array/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+
+        int j = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (nums[j] != nums[i]) {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+
+        return j;
+    }
+}
