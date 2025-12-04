a32200efdccb80ac1eb6bdbfb4469f466d804862
Dhwanil Ditani <44235738+dhwanil-ditani@users.noreply.github.com>
2025-12-04 08:31:50 +0000

    update

---
+++ Linked-List/Logic Building/Segregate odd and even nodes in LL/Optimal.java
@@ -0,0 +1,45 @@
+/*
+Definition for singly linked list:
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+*/
+
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode odd = head;
+        ListNode even = head.next;
+        ListNode firstEven = head.next;
+
+        while (even != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+
+        odd.next = firstEven;
+
+        return head;
+    }
+}
