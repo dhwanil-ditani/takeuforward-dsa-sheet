0e41ba8a62f1e10d3c1f585fb3b99c5061c42398
Dhwanil Ditani <ditanidhwanil@gmail.com>
2025-12-23 10:32:06 +0530

    update

--- Linked-List/Logic Building/Remove Nth node from the back of the LL/Solution.java
+++ Linked-List/Logic Building/Remove Nth node from the back of the LL/Solution.java
@@ -1,3 +1,45 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+        val = 0;
+        next = null;
+    }
+
+    ListNode(int data1) {
+        val = data1;
+        next = null;
+    }
+
+    ListNode(int data1, ListNode next1) {
+        val = data1;
+        next = next1;
+    }
+}
+
 class Solution {
-    
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode one = head;
+        ListNode two = head;
+
+        int i = 0;
+        while (i < n) {
+            one = one.next;
+            i++;
+        }
+
+        if (one == null) {
+            return head.next;
+        }
+
+        while (one.next != null) {
+            one = one.next;
+            two = two.next;
+        }
+
+        two.next = two.next.next;
+
+        return head;
+    }
 }
